4d0a840d5e5f0846eed2cdd49482d1aae83df543
Valeriia Bokher <valeriiabokher@gmaol.com>
2023-04-12 18:08:32 +0300

    add

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,56 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	// Если параметры не переданы, создаем пустой объект параметров
+	options = options || {};
+
+	// Устанавливаем значения по умолчанию для параметров
+	var repeatTimes = options.repeatTimes || 1;
+	var separator = options.separator || '+';
+	var addition = options.addition !== undefined ? options.addition : '';
+	var additionRepeatTimes = options.additionRepeatTimes || 1;
+	var additionSeparator = options.additionSeparator || '|';
+
+	// Приводим str и addition к строковому типу
+	str = String(str);
+	addition = String(addition);
+
+	// Строим повторяющуюся строку addition
+	var repeatedAddition = '';
+	for (var i = 0; i < additionRepeatTimes; i++) {
+		repeatedAddition += addition;
+		if (i !== additionRepeatTimes - 1) {
+			repeatedAddition += additionSeparator;
+		}
+	}
+
+	// Строим повторяющуюся строку str с добавлением повторяющейся строки addition
+	var repeatedString = '';
+	for (var j = 0; j < repeatTimes; j++) {
+		repeatedString += str;
+		repeatedString += repeatedAddition;
+		if (j !== repeatTimes - 1) {
+			repeatedString += separator;
+		}
+	}
+
+	return repeatedString;
 }
 
 module.exports = {
-  repeater
+	repeater,
 };
